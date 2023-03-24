2a71de9bcdbab51d97040b905d5b206d86fb06bb
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2023-03-24 17:47:40 +0100

    guay

--- www/script.js
+++ www/script.js
@@ -1,1 +1,37 @@
 /*Contendrá el código JavaScript con las funciones a implementar para proporcionar las funcionalidades deseadas. */
+const taskList = [];
+
+const taskListElem = document.querySelector("#tasklist");
+
+const loadTasks = async () => {
+  try {
+    const response = await fetch("/tasks/get");
+    const data = await response.json();
+    console.log(data);
+    data.forEach( item => {
+      const li = document.createElement("li");
+      li.innerHTML = item.title;
+      taskListElem.appendChild(li);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  
+}
+
+loadTasks();
+
+const add = () => {}
+
+const remove = () => {}
+
+const toggleDone = () => {}
+
+const addButton = document.querySelector("#fab-add");
+
+addButton.addEventListener("touchend", add);
+
+
+
+
+

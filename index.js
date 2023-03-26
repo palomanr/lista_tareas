e0a01cff3879ba558517f17d64fe44a7b21bbb9c
Paloma Núñez Reyes <palomanunezreyes@gmail.com>
2023-03-26 14:58:38 +0200

    hecho

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-/*Contendrá la lógica para ejecutar nuestro servidor Web HTTPS.*/
+/* Este código es un servidor web básico que maneja solicitudes HTTPS. Se utiliza la biblioteca de Node.js 'http' para crear el servidor web y 'fs' para leer y escribir archivos.
+
+La constante 'PORT' establece el número del puerto en el que el servidor web escucha. En este caso, el servidor escucha en el puerto 80.
+
+La función 'serveStaticFile' lee el archivo especificado y devuelve una promesa que resuelve en los datos del archivo leído.
+
+La función 'sendResponse' establece los encabezados de respuesta HTTP y envía el contenido de respuesta.
+
+La función 'handleRequest' maneja las solicitudes entrantes. Si el método HTTP es GET, se utiliza una instrucción 'switch' para determinar la ruta solicitada y servir el archivo correspondiente en caso de que la ruta sea válida. Si la ruta no es válida, se envía una respuesta de error al cliente. Si el método HTTP no es GET, se devuelve un error 405 "Método no permitido".
+
+Finalmente, el servidor se crea utilizando la función 'createServer' de la biblioteca 'http' y se establece para escuchar el puerto especificado. */
+
 const http = require('http');
 const fs = require('fs');
 
@@ -38,6 +49,29 @@ const handleRequest = async (request, response) => {
         content = await serveStaticFile("www/style.css");
         contentType = "text/css";
         break;
+      case "/tasklist/update":
+        if (request.headers["content-type"] === "application/json") {
+          let body = "";
+          request.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+          request.on("end", async () => {
+            try {
+              const tasks = JSON.parse(body);
+              fs.writeFile("tasks.json", JSON.stringify(tasks), (err) => {
+                if (err) throw err;
+                console.log("Tasks updated successfully!");
+                sendResponse(response, JSON.stringify({ message: "Tasks updated successfully!" }), "application/json");
+              });
+            } catch (err) {
+              console.log(err);
+              sendResponse(response, JSON.stringify({ message: "Error updating tasks!" }), "application/json");
+            }
+          });
+        } else {
+          sendResponse(response, JSON.stringify({ message: "Invalid content type!" }), "application/json");
+        }
+        break;
       default: 
         content = "Ruta no v&aacutelida\r\n";
         contentType = "text/html";
@@ -49,6 +83,7 @@ const handleRequest = async (request, response) => {
      response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
   }
 }
+
 
 
 const server = http.createServer(handleRequest);
--- www/script.js
+++ www/script.js
@@ -1,14 +1,21 @@
-/*Contendrá el código JavaScript con las funciones a implementar para proporcionar las funcionalidades deseadas. */
+/* Este es un código de JavaScript que utiliza Fetch API y manipula el DOM para crear una lista de tareas en una página web.*/
 const taskList = [];
 
 const taskListElem = document.querySelector("#tasklist");
+
+/* Primero, se crea un array vacío llamado taskList, y se selecciona un elemento HTML con el ID "tasklist" utilizando querySelector. Esto se hace para poder agregar y eliminar elementos de la lista de tareas.
+
+Luego se define una función asíncrona llamada loadTasks. Esta función utiliza la función fetch para obtener datos desde la URL "/tasks/get", y luego utiliza el método json para convertir la respuesta en un objeto JSON. A continuación, se recorre cada elemento del objeto JSON utilizando el método forEach, y se crea un elemento HTML <li> con el título de la tarea y se agrega a la lista de tareas con appendChild. Esta función también maneja cualquier error que pueda ocurrir al obtener los datos utilizando un bloque try/catch.
+
+Después se llama la función loadTasks para cargar la lista de tareas desde el servidor. 
+*/
 
 const loadTasks = async () => {
   try {
     const response = await fetch("/tasks/get");
     const data = await response.json();
     console.log(data);
-    data.forEach( item => {
+    data.forEach(item => {
       const li = document.createElement("li");
       li.innerHTML = item.title;
       taskListElem.appendChild(li);
@@ -16,87 +23,131 @@ const loadTasks = async () => {
   } catch (err) {
     console.log(err);
   }
-  
-}
+};
 
 loadTasks();
 
-const add = () => {
-    const input = document.getElementById('new-task');
-    const title = input.value.trim();
-  
-    if (title !== '') {
-      const newList = {
-        id: taskList.length + 1,
-        title,
-        done: false
-      };
-  
-      taskList.push(newList);
-      input.value = '';
-      renderTodos();
-    }
-}
+/* Hay una función asíncrona llamada updateTasksOnServer. 
+Esta función utiliza fetch para enviar una solicitud POST a la URL "/tasklist/update"
+ con el array taskList convertido a formato JSON en el cuerpo de la solicitud. 
+ También se especifica que el tipo de contenido es JSON en los encabezados. La respuesta
+  del servidor también se convierte a un objeto JSON utilizando json, y se imprime 
+  en la consola con console.log. Al igual que loadTasks, esta función también maneja 
+  cualquier error que pueda ocurrir utilizando try/catch. 
+*/
+const updateTasksOnServer = async () => {
+  try {
+    const response = await fetch("/tasklist/update", {
+      method: "POST",
+      body: JSON.stringify(taskList),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-add();
+/* Hay una función asíncrona llamada add. Esta función se llama cuando se hace clic en el botón "Agregar" (identificado por el elemento HTML con el ID "fab-add").
+ Primero, se selecciona el elemento de entrada de texto para obtener el valor del nombre 
+ de la tarea. Si el valor no está vacío, se crea un nuevo objeto de tarea con un ID único, 
+ el título de la tarea y el valor booleano "hecho" establecido en falso. Este objeto se 
+ agrega al array taskList, y se crea un elemento HTML <li> con el título de la tarea y se 
+ agrega a la lista de tareas con appendChild. Luego se borra el valor del campo de entrada 
+ de texto y se llama a la función updateTasksOnServer. */
+const add = async () => {
+  const taskNameInput = document.querySelector("#task-name");
+  const taskName = taskNameInput.value.trim();
+  if (taskName !== "") {
+    const newTask = {
+      id: taskList.length + 1,
+      title: taskName,
+      done: false
+    };
+    taskList.push(newTask);
+    const li = document.createElement("li");
+    li.innerHTML = newTask.title;
+    li.dataset.id = newTask.id;
+    taskListElem.appendChild(li);
+    taskNameInput.value = "";
+    await updateTasksOnServer();
+  }
+};
 
+/*Remove se llama cuando se desliza un elemento de la lista de tareas hacia la derecha. 
+La función busca el índice del elemento en el array taskList, y si lo encuentra, elimina 
+ese elemento del array utilizando el método splice. También elimina el elemento correspondiente 
+del DOM utilizando remove, y llama a updateTasksOnServer para actualizar los datos en el servidor.*/
+const remove = async id => {
+  const taskIndex = taskList.findIndex(task => task.id === id);
+  if (taskIndex !== -1) {
+    taskList.splice(taskIndex, 1);
+    const taskElem = document.querySelector(`li[data-id="${id}"]`);
+    taskElem.remove();
+    await updateTasksOnServer();
+  }
+};
 
-
-const remove = () => {
-    tasks = taskList.filter(task => task.id !== id);
-    renderTodos();
-}
-
-remove();
-
-const toggleDone = () => {
-    tasks = tasks.map(task  => {
-        if (task.id === id) {
-          return {
-            ...task ,
-            done: !task.done
-          };
-        } else {
-          return task;
-        }
-      });
-      renderTodos();
-}
-
-toggleDone();
+/*toggleDone se llama cuando se toca un elemento de la lista de tareas durante un cierto tiempo.
+ La función busca el índice del elemento en el array taskList, y si lo encuentra, cambia el valor 
+ de la propiedad "hecho" del objeto tarea utilizando el operador de negación. 
+También cambia la clase CSS del elemento correspondiente para mostrar si está hecho o no, y llama 
+a updateTasksOnServer para actualizar los datos en el servidor.*/
+const toggleDone = async id => {
+  const taskIndex = taskList.findIndex(task => task.id === id);
+  if (taskIndex !== -1) {
+    taskList[taskIndex].done = !taskList[taskIndex].done;
+    const taskElem = document.querySelector(`li[data-id="${id}"]`);
+    taskElem.classList.toggle("done");
+    await updateTasksOnServer();
+  }
+};
 
 const addButton = document.querySelector("#fab-add");
 
 addButton.addEventListener("touchend", add);
 
+taskListElem.addEventListener("touchstart", event => {
+  const touch = event.touches[0];
+  const taskElem = touch.target.closest("li");
+  if (taskElem) {
+    const taskId = parseInt(taskElem.dataset.id);
+    taskElem.dataset.startX = touch.clientX;
+    taskElem.dataset.startY = touch.clientY;
+    taskElem.dataset.startTime = Date.now();
+    taskElem.dataset.touched = true;
+    setTimeout(() => {
+      if (taskElem.dataset.touched === "true") {
+        toggleDone(taskId);
+      }
+    }, 2000);
+  }
+});
 
-
-function renderTodos() {
-  const list = document.getElementById('todos');
-  list.innerHTML = '';
-
-  todos.forEach(task  => {
-    const li = document.createElement('li');
-    li.textContent = task.title;
-
-    if (task.done) {
-      li.classList.add('done');
+taskListElem.addEventListener("touchmove", event => {
+  const touch = event.touches[0];
+  const taskElem = touch.target.closest("li");
+  if (taskElem && taskElem.dataset.touched === "true") {
+    const deltaX = touch.clientX - taskElem.dataset.startX;
+    const deltaY = touch.clientY - taskElem.dataset.startY;
+    const absDeltaX = Math.abs(deltaX);
+    const absDeltaY = Math.abs(deltaY);
+    if (absDeltaX > 10 && absDeltaX > absDeltaY) {
+      taskElem.dataset.touched = false;
+      if (deltaX > 0) {
+        remove(parseInt(taskElem.dataset.id));
+      }
     }
+  }
+});
 
-    li.addEventListener('click', () => toggleDone(todo.id));
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Eliminar';
-    deleteButton.addEventListener('click', () => remove(todo.id));
-
-    li.appendChild(deleteButton);
-    list.appendChild(li);
-  });
-}
-
-
-
-
-
-
-
+taskListElem.addEventListener("touchend", event => {
+  const touch = event.changedTouches[0];
+  const taskElem = touch.target.closest("li");
+  if (taskElem) {
+    taskElem.dataset.touched = false;
+  }
+});
